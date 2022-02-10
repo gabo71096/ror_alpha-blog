df0531788d68a0a4bc4882cdf5c3daceb417e439
Gabriel Lopez <gabo_71096@hotmail.com>
2022-02-10 20:01:34 -0300

    add user to article before validation to bypass error

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,10 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+
+  before_validation :add_user
+
+  def add_user
+    self.user = User.first
+  end
 end
