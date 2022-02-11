2700d7387cee02fc9d4dcce7364775dbfb3ce4e8
Gabriel Lopez <gabo_71096@hotmail.com>
2022-02-11 18:56:44 -0300

    downcase email before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
   has_many :articles
+
+  before_save { self.email = email.downcase }
 end
