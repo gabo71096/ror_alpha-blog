8e7301d3322698953b53cda6497674e2de34ea9f
Gabriel Lopez <gabo_71096@hotmail.com>
2022-02-11 19:50:06 -0300

    add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'about', to: 'home#about'
 
+  get 'signup', to: 'users#new'
+
+  resources :users, except: [:new]
+
   resources :articles
 end
