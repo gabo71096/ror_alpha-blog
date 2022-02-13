4cfe0116767a94d6a6014dca8e84211aba6142cf
Gabriel Lopez <gabo_71096@hotmail.com>
2022-02-13 13:55:36 -0300

    add gravatar_for helper for img avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@
 
 # Application Helper
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    email_address = user.email.downcase
+    email_hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    image_src = "https://www.gravatar.com/avatar/#{email_hash}?s=#{size}"
+    image_tag(image_src, alt: user.username, class: 'rounded shadow d-block mx-auto mb-5')
+  end
 end
