7332a4e5b12402b562a4f14ea108e5c3dd962044
Gabriel Lopez <gabo_71096@hotmail.com>
2022-02-13 12:36:46 -0300

    define create and user_params methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'Thanks for sign up'
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
+  end
 end
