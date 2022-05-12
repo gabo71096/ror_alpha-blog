a3a6643791935051c0d2ae93972795213e024e54
Gabriel Lopez <gabo_71096@hotmail.com>
2022-05-11 21:01:43 -0400

    move session methods to application controller, expose them using helper_method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,4 @@ module ApplicationHelper
     image_src = "https://www.gravatar.com/avatar/#{email_hash}?s=#{size}"
     image_tag(image_src, alt: user.username, class: 'rounded shadow-sm d-block mx-auto')
   end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
 end
