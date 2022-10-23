12976e037501dc5fdee2f489a8fd8c77b33e84b1
Gabriel Lopez <gabo_71096@hotmail.com>
2022-10-23 12:32:57 -0300

    implement category creation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,27 @@
 
 # Categories controller
 class CategoriesController < ApplicationController
-  def new; end
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = 'Category created'
+      redirect_to @category
+    else
+      render('new')
+    end
+  end
+
   def index; end
+
   def show; end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
